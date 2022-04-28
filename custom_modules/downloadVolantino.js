82ebabfb9d83cf764ac081e9ad9bbde8c9b2e3aa
DanieleNovarese <daniele@wedosport.net>
2022-04-28 22:01:31 +0200

    awdaw

---
+++ custom_modules/downloadVolantino.js
@@ -0,0 +1,10 @@
+const https = require('https');
+const fs = require('fs');
+
+function downloadVolantino(){
+    const path = "result/result.txt";
+
+    https.get(url,function(res){
+        
+    })
+}
